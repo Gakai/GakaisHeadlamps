c9a41f924d9299583723ba8d6797d0e064d5c8c7
Gakai <github@skutscher.de>
2015-06-22 03:32:40 +0200

    Changed to release 1.0

--- src/main/java/de/gakai/headlamps/HeadlampMod.java
+++ src/main/java/de/gakai/headlamps/HeadlampMod.java
@@ -7,11 +7,11 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = HeadlampMod.MODID, version = HeadlampMod.VERSION)
+@Mod(modid = HeadlampMod.MODID, version = HeadlampMod.VERSION, dependencies = "required-after:DynamicLights")
 public class HeadlampMod
 {
     public static final String MODID = "GakaisHeadlamps";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "1.0";
 
     static final HeadlampItem ITEM_HEADLAMP_CHAIN = new HeadlampItem(ArmorMaterial.CHAIN);
     static final HeadlampItem ITEM_HEADLAMP_IRON = new HeadlampItem(ArmorMaterial.IRON);
